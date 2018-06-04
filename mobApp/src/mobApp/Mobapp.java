fc266166d12919aa2a778df5d1290018d8819b98
pd3chae <pendr_3chae@me.com>
2018-06-05 04:20:15 +0900

    update mobApp

---
+++ mobApp/src/mobApp/Mobapp.java
@@ -0,0 +1,140 @@
+package mobApp;
+
+import java.net.URI;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.utils.URIBuilder;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
+import org.eclipse.paho.client.mqttv3.MqttCallback;
+import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class Mobapp {
+	public static int requestId = 0;
+	public static String[] pcon = new String[2];
+	public static int ae = 0;
+
+	public static void main(String[] args) {
+		final String MQTT_BROCKER_IP = "tcp://localhost:1883";
+		pcon[0] = "";
+		pcon[1] = "";
+		
+		try {
+			MqttClient client = new MqttClient(
+					MQTT_BROCKER_IP,
+					MqttClient.generateClientId(),
+					new MemoryPersistence());
+			client.connect();
+			
+			client.setCallback(new MqttCallback() {
+				@Override
+				public void connectionLost(Throwable cause) {
+				}
+				@Override
+				public void deliveryComplete(IMqttDeliveryToken arg0) {
+				}
+				@Override
+				public void messageArrived(String arg0, MqttMessage arg1) {
+					String con = "";
+					String[] a = null;
+					
+					System.out.println(arg1.toString());
+					JSONObject tmp;
+					try {
+						tmp = new JSONObject(arg1.toString());
+						tmp = tmp.getJSONObject("pc").getJSONObject("sgn").getJSONObject("nev").getJSONObject("rep").getJSONObject("m2m:cin");
+						con = tmp.get("con").toString();
+						a = con.split(" ");
+						if(a[1].equals("knu-led1"))
+							ae = 0;
+						else if(a[1].equals("knu-led2"))
+							ae = 1;
+					} catch (JSONException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					}
+					if(pcon[0].equals(con) || pcon[1].equals(con))
+						return;
+					try {
+						contentInstanceCreateRequest(a[1], con);
+					} catch (Exception e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				}
+			});
+			client.subscribe("/oneM2M/req/rosemary/#", 1);
+		}
+		catch(MqttException e) {
+			e.printStackTrace();
+		}
+	
+	}
+	
+	public static int contentInstanceCreateRequest(String cnt, String content) throws Exception {
+		MysqlHelper sqlHelper = new MysqlHelper("192.168.1.13", "3306", "mobiusdb", "root", "dbsckd");
+		String aei = sqlHelper.selectColumn("ae", "aei");
+		
+		System.out.println("[&CubeThyme] \"" + cnt + "\"'s contentInstance create request.......");
+		
+		String requestBody = "{ \"m2m:cin\": {" + 
+				"\"cnf\": \""+"text/plain:0"+"\"," + 
+				"\"con\": \""+content+"\"" + 
+				"}" + 
+				"}";
+		
+		StringEntity entity = new StringEntity(
+						new String(requestBody.getBytes()));
+		System.out.println("YC_Reqbody: "+requestBody);
+
+		URI uri = new URIBuilder()
+				.setScheme("http")
+				.setHost("127.0.0.1" + ":" + 7579)
+				.setPath("/" + "Mobius/" + "knu-ae" + "/" + cnt)
+				.setParameter("rcn","0")
+				.build();
+		
+		System.out.println("YC_HttpCli_uri: "+uri.toString());
+		
+		HttpPost post = new HttpPost(uri);
+		post.setHeader("Content-Type", "application/vnd.onem2m-res+json;ty=4");
+		post.setHeader("X-M2M-Origin", aei);
+		
+		post.setHeader("X-M2M-RI", "nCubeThyme" + Integer.toString(requestId++));
+		post.setEntity(entity);
+
+		System.out.println("YC_HttpCli_post: "+post.toString());
+		System.out.println("YC_postEntity: "+entity.toString());
+				
+		CloseableHttpClient httpClient = HttpClients.createDefault();
+		
+		HttpResponse response = httpClient.execute(post);
+		System.out.println("YC_HttpCli_response: "+response.toString());
+	
+		HttpEntity responseEntity = response.getEntity();
+		System.out.println("YC_HttpCli_responseEnity: "+responseEntity.toString());
+
+		String responseString = EntityUtils.toString(responseEntity);
+
+		int responseCode = response.getStatusLine().getStatusCode();
+		
+		System.out.println("[&CubeThyme] contentInstance create HTTP Response Code : " + responseCode);
+		System.out.println("[&CubeThyme] contentInstance create HTTP Response String : " + responseString);
+		
+		httpClient.close();
+		
+		return responseCode;
+	}
+	
+	
+}
